b18f5f4e2a40519a6c64dce3162206c2069fa709
vkyarain <engr_waqar_arain@yahoo.com>
2025-09-15 01:10:18 +0500

    revert command testing

--- main.php
+++ main.php
@@ -1,10 +1,38 @@
 <?php
 echo 'This is my PHP file';
+
+
+print
+echo 3333;
+
 $vafsdf =   33;
 
 function asfd(){
     echo 'This is my function';
     exit;
 }
+
+isset();
+
+if(isset($vafsdf) && ! $sdfg)
+{
+    echo 'waqar';
+}
+NULL
+
+asfd();
+
+
+//asdfasfasdfasf
+
+/*
+if(isset($vafsdf))
+{
+    echo 'waqar';
+}
+*/
+
+#dsfgsdfg s
+
 
 ?>
