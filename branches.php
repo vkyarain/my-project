0d4e99bd8908dbf3f31abe86fd8c3c106645eb07
vkyarain <engr_waqar_arain@yahoo.com>
2025-09-15 03:44:12 +0500

    second branch created

---
+++ branches.php
@@ -0,0 +1,7 @@
+git branch second                   // create 'second' branch
+git branch                          //check branch 
+git checkout second                 //branch switch
+git switch second                   //branch switch
+git checkout -b third               //Check if branch doesnot exists then Create branch and switch in it
+git switch -c four                  //Check if branch doesnot exists then Create branch and switch in it (switch is modern commnd)
+git branch -D four                  //Delete 'four' branch
