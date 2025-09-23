dc8d0e0dea930d5ebcd461545e9de3c74c3343de
vkyarain <engr_waqar_arain@yahoo.com>
2025-09-24 04:21:34 +0500

    branch four created

---
+++ branches.php
@@ -0,0 +1,8 @@
+git branch second                   // create 'second' branch
+git branch                          //check branch 
+git checkout second                 //branch switch
+git switch second                   //branch switch
+git checkout -b third               //Check if branch doesnot exists then Create branch and switch in it
+git switch -c four                  //Check if branch doesnot exists then Create branch and switch in it (switch is modern commnd)
+git branch -D four                  //Delete 'four' branch
+git push -u origin four             //Create branch on github and push code into four branch at github
